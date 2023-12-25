be0fa3448fa84000746510c773c7574bce690f05
zy7y <13271962515@163.com>
2023-12-25 22:43:06 +0800

    #4 生成通用模型 (#5)
    * fix#1 生成router.py中导入其他model类
    
    * feat#4 生成通用响应模型，分页模型

--- generate/repository.py
+++ generate/repository.py
@@ -1,11 +1,11 @@
 imports = {
     "from typing import List, Optional",
-    "from sqlmodel import Session, SQLModel,select",
+    "from sqlmodel import Session, SQLModel,select, func",
 }
 
 template = """
     @classmethod
-    def create(cls, session: Session, obj_in: {table_name}DO) -> "{table_name}":
+    def create(cls, session: Session, obj_in: {table_name}DTO) -> "{table_name}":
         obj = cls(**obj_in.dict())
         session.add(obj)
         session.commit()
@@ -17,7 +17,7 @@ template = """
         return session.get(cls, id)
 
     @classmethod
-    def update(cls, session: Session, id: int, obj_in: {table_name}DO) -> Optional["{table_name}"]:
+    def update(cls, session: Session, id: int, obj_in: {table_name}DTO) -> Optional["{table_name}"]:
         obj = cls.get_by_id(session, id)
         if obj:
             for field, value in obj_in.dict(exclude_unset=True).items():
@@ -34,9 +34,16 @@ template = """
             session.delete(obj)
             session.commit()
         return obj
+    
+    @classmethod
+    def count(cls, session: Session, **kwargs):
+        return session.scalar(
+  select(func.count()).
+  select_from({table_name}).filter_by(**kwargs)
+)
 
     @classmethod
-    def find_with_pagination(cls, session: Session, page: int, page_size: int, **kwargs) -> List["{table_name}"]:
-        stmt = select(cls).where(**kwargs).offset((page - 1) * page_size).limit(page_size)
-        return session.exec(stmt).all()
+    def query_all_by_limit(cls, session: Session, page_number: int, page_size: int, **kwargs):        
+        stmt = select(cls).filter_by(**kwargs).offset((page_number - 1) * page_size).limit(page_size)
+        return dict(data=session.exec(stmt).all(), total=cls.count(session, kwargs))
 """
--- generate/router.py
+++ generate/router.py
@@ -5,50 +5,49 @@ template = """
 
 
 @{table_name}_router.get("/{id}", summary="通过ID查询详情")
-def query_{table_name}_by_id(id: int) -> Optional[{model}]:
+def query_{table_name}_by_id(id: int) -> Result[{model}]:
     with Session(engine) as session:
-        return {model}.get_by_id(session, id)
+        return Result.ok({model}.get_by_id(session, id))
 
 
 @{table_name}_router.get("", summary="分页条件查询")
-def query_{table_name}_all_by_limit(limit: int = 1, offset: int = 10) -> List[{model}]:
+def query_{table_name}_all_by_limit(query: {model}Query = Depends()) -> PageResult[{model}]:
     with Session(engine) as session:
-        return {model}.find_with_pagination(session, limit, offset)
+        return PageResult.ok({model}.query_all_by_limit(session, **query.model_dump(exclude_unset=True)))
 
 
 @{table_name}_router.post("", summary="新增数据")
-def create_{table_name}(instance: {model_do}) -> {model}:
+def create_{table_name}(instance: {model_do}) -> Result[{model}]:
     with Session(engine) as session:
-        return {model}.create(session, instance)
+        return Result.ok({model}.create(session, instance))
 
 
 @{table_name}_router.patch("/{id}", summary="更新数据")
-def update_{table_name}_by_id(id: int, instance: {model_do}) -> {model}:
+def update_{table_name}_by_id(id: int, instance: {model_do}) -> Result[{model}]:
     with Session(engine) as session:
-        return {model}.update(session, id, instance)
+        return Result.ok({model}.update(session, id, instance))
 
 
 @{table_name}_router.delete("/{id}", summary="删除数据")
-def delete_{table_name}_by_id(id: int) -> Optional[{model}]:
+def delete_{table_name}_by_id(id: int) -> Result[{model}]:
     with Session(engine) as session:
-        return {model}.delete_by_id(session, id)
+        return Result.ok({model}.delete_by_id(session, id))
 """
 
 
 def render(table_name):
     pascal_table_name = to_pascal(table_name)
     import_code = f"""
-from typing import Optional, List
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlmodel import Session
-from model import {pascal_table_name}DO, {pascal_table_name}
+from model import {pascal_table_name}DTO, {pascal_table_name}, Result, PageResult, {pascal_table_name}Query
 from db import engine
         """
 
     router_code = template.format(
         table_name=table_name,
         model=pascal_table_name,
-        model_do=pascal_table_name + "DO",
+        model_do=pascal_table_name + "DTO",
         id="{id}",
     )
     return import_code + router_code
