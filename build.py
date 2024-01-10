9c8fec5ece7f0546cfcee65b5cdd36123848fff8
zy7y <13271962515@163.com>
2024-01-11 00:39:22 +0800

    ci: icon or icns

--- build.py
+++ build.py
@@ -9,7 +9,6 @@ params = [
     '--add-data', f'{yapf_third_party.__file__.replace("__init__.py", "")}:yapf_third_party',
     '--clean',
     '--noconfirm',
-    '--icon', 'static/logo.icns',
     'client.py',
 ]
 
@@ -17,8 +16,13 @@ current_os = platform.system()
 
 from PyInstaller import __main__ as pyi
 
-pyi.run(params)
+params.append("--icon"),
 if current_os == "Windows":
+    params.append("static/logo.icns")
     path = "./dist/client.exe"
 else:
+    params.append("static/logo.icon")
     path = "./dist/client"
+
+pyi.run(params)
+print(path)
